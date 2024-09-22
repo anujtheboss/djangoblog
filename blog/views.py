e3ad17afb329a460255e092af49579e5d28c4fd6
birendra-dev <077bct025@ioepc.edu.np>
2024-09-22 16:44:10 +0545

    adding search-bar

--- blog/views.py
+++ blog/views.py
@@ -8,21 +8,60 @@ from django.views.generic import (ListView,
                                  DeleteView,
                                  )
 from .models import Post
+from django.db.models import Q
 
-
-def home(request):
-    context = {
-        'posts': Post.objects.all()
-    }   
-    return render(request, 'blog/home.html',context)
+# def home(request):
+#     q = request.GET.get('q') if request.GET.get('q') else ''
+    
+#     if q:
+#         # Filter posts by title, content, or author_username
+#         posts = Post.objects.filter(
+#             Q(title__icontains=q) | 
+#             Q(content__icontains=q) | 
+#             Q(author__username__icontains=q)
+#         )
+#     else:
+#         # No query, so return all posts
+#         posts = Post.objects.all()
+    
+#     context = {
+#         'posts': posts,
+#         'q': q
+#     }
+    
+#     return render(request, 'blog/home.html', context)
 
 
 class PostListView(ListView):
     model = Post
-    template_name = 'blog/home.html'     #<app>/<model>_<viewtype>.html
+    template_name = 'blog/home.html'  # <app>/<model>_<viewtype>.html
     context_object_name = 'posts'
-    ordering = ['-date_posted']          # to oldest to newest ['-date_posted']
+    ordering = ['-date_posted']
     paginate_by = 5
+
+    def get_queryset(self):
+        # Get the search query from the request
+        q = self.request.GET.get('q', '')
+
+        if q:
+            # Filter posts by title, content, or author's username
+            return Post.objects.filter(
+                Q(title__icontains=q) |
+                Q(content__icontains=q) |
+                Q(author__username__icontains=q)
+            ).order_by('-date_posted')
+        else:
+            # If no search query, return all posts ordered by date
+            return Post.objects.all().order_by('-date_posted')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['q'] = self.request.GET.get('q', '')  # Pass 'q' to the template
+
+        # Add the total post count to the context
+        context['post_count'] = self.get_queryset().count()
+        return context
+
 
 class UserPostListView(ListView):
     model = Post
